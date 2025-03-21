782f46e5bae67af4e929c0bc9aff9946121b7238
Ana Claudia <anacnogueira@gmail.com>
2025-03-21 19:28:47 -0300

    Apresentando o resultado na modal

--- script.js
+++ script.js
@@ -2,10 +2,23 @@
 const form = document.querySelector("form");
 const inputWeight = document.querySelector("#weight");
 const inputHeight = document.querySelector("#height");
+const modalWrapper = document.querySelector(".modal-wrapper");
+const buttonClose = document.querySelector(".close");
 
 form.onsubmit = (event) => {
   event.preventDefault();
 
   const weight = inputWeight.value;
   const height = inputHeight.value;
+
+  const result = calculateIMC(weight, height);
+  modalWrapper.querySelector(".title").innerText = `Seu IMC é de ${result}`;
+
+  modalWrapper.classList.add("open");
 };
+
+buttonClose.onclick = () => modalWrapper.classList.remove("open");
+
+function calculateIMC(weight, height) {
+  return (weight / (height / 100) ** 2).toFixed(2);
+}
