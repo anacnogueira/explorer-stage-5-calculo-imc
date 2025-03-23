71f5ca34e056edce3de1350bf1857e209c8e2da5
Ana Claudia <anacnogueira@gmail.com>
2025-03-23 17:52:39 -0300

    Modularizando o Modal popup

--- js/script.js
+++ js/script.js
@@ -1,19 +1,9 @@
+import { Modal } from "./modal.js";
+
 // variaveis
 const form = document.querySelector("form");
 const inputWeight = document.querySelector("#weight");
 const inputHeight = document.querySelector("#height");
-
-const Modal = {
-  wrapper: document.querySelector(".modal-wrapper"),
-  message: document.querySelector(".modal .title"),
-  btnClose: document.querySelector(".modal button.close"),
-  open() {
-    Modal.wrapper.classList.add("open");
-  },
-  close() {
-    Modal.wrapper.classList.remove("open");
-  },
-};
 
 form.onsubmit = (event) => {
   event.preventDefault();
@@ -26,8 +16,6 @@ form.onsubmit = (event) => {
 
   Modal.open();
 };
-
-Modal.btnClose.onclick = () => Modal.close();
 
 function calculateIMC(weight, height) {
   return (weight / (height / 100) ** 2).toFixed(2);
