4dea90fe03c601333eaf4e3469b141f360e4d9f7
Ana Claudia <anacnogueira@gmail.com>
2025-03-24 13:49:16 -0300

    Modularisando a caixa de alerta de erro

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 import { Modal } from "./modal.js";
+import { AlertError } from "./alert-error.js";
 
 // variaveis
 const form = document.querySelector("form");
@@ -14,9 +15,11 @@ form.onsubmit = (event) => {
   const showAlertError = notNumber(weight) || notNumber(height);
 
   if (showAlertError) {
-    console.log("Mostrat o alerta de erro");
+    AlertError.open();
     return;
   }
+
+  AlertError.close();
 
   const result = calculateIMC(weight, height);
   Modal.message.innerText = `Seu IMC é de ${result}`;
