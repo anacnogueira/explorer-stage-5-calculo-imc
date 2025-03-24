c2c4b88122f63a854c68128833f8ddc32670c4ec
Ana Claudia <anacnogueira@gmail.com>
2025-03-24 09:43:16 -0300

    Validando erros

--- js/script.js
+++ js/script.js
@@ -11,11 +11,22 @@ form.onsubmit = (event) => {
   const weight = inputWeight.value;
   const height = inputHeight.value;
 
+  const showAlertError = notNumber(weight) || notNumber(height);
+
+  if (showAlertError) {
+    console.log("Mostrat o alerta de erro");
+    return;
+  }
+
   const result = calculateIMC(weight, height);
   Modal.message.innerText = `Seu IMC é de ${result}`;
 
   Modal.open();
 };
+
+function notNumber(value) {
+  return isNaN(value) || value.trim() === "";
+}
 
 function calculateIMC(weight, height) {
   return (weight / (height / 100) ** 2).toFixed(2);
